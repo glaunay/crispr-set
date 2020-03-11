79d098045de5eec63a75c0a67639523b15e31d8b
glaunay <pitooon@gmail.com>
2020-03-11 12:35:05 +0100

    extra_compilation_flag

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from distutils.core import setup, Extension
 
-module1 = Extension('twobits',
-                    sources = ['./two_bits_encoding.c', 'twobitsmodule.c'])
+module = Extension('twobits',
+                     sources = ['./two_bits_encoding.c', 'twobitsmodule.c'],
+                     extra_compile_args=['-pedantic', '-std=c99'],
+                     include_dirs = ['./include']
+                     )
 
 setup (name = 'twobits',
        author_email = 'pitooon@gmail.com',
-	version = '1.3',
+	version = '1.4',
        #define_macros = [('MAJOR_VERSION', '1'),
        #                              ('MINOR_VERSION', '0')],
        description = 'Encodind/decoding/Truncating k-mers in 2bits-per-base ',
-       include_dirs = ['./include'],
-       ext_modules = [module1])
+       ext_modules = [module])
 
